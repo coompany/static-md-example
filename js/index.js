90ba3236d56e482264af3204030a8dd41260a6f6
Andrea Jemmett <andreajemmett@gmail.com>
2017-09-14 17:22:30 +0200

    fix indentation & .editorconfig

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(($) => {
-	for (let chunk of CHUNKS) {
-		loadChunk(chunk, (response) => {
+  for (let chunk of CHUNKS) {
+    loadChunk(chunk, (response) => {
       const md = atob(response.content);
-			const html = converter.makeHtml(md);
-			$('#' + chunk).html(html);
-		});
-	}
+      const html = converter.makeHtml(md);
+      $('#' + chunk).html(html);
+    });
+  }
 });
